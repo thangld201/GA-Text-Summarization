c6f5aa053c6cbe165173ba7c8575b7d6d2f6d171
William Chen <wchen6255@knights.ucf.edu>
2021-04-22 19:22:17 -0400

    properly score summarizations

--- src/ga.py
+++ src/ga.py
@@ -8,7 +8,7 @@ def summarize(dictionary, corpus, threshold):
     tokenizer = MosesTokenizer(lang='en')
     summary = list()
 
-    with open(corpus, 'r') as file:
+    with open(corpus, 'r', encoding='utf-8') as file:
         corpus_lines = file.readlines()
 
         for line in corpus_lines:
@@ -19,29 +19,34 @@ def summarize(dictionary, corpus, threshold):
             for word in line_tok:
                 if word.lower() in dictionary:
                     sentence_score += dictionary[word.lower()]
-                else:
-                    sentence_score += 0.0
-            if sentence_score > threshold:
-                summary.append(line)
-
+                if sentence_score / len(line_tok) > threshold:
+                    summary.append(line)
+                    break
+    print(summary)
     return summary
 
-def score(summary, reference):
+def score_summary(summary, reference_file):
     avg = 0.0
+    rouge = Rouge()
+    reference = open(reference_file, 'r', encoding='utf-8').readlines()
+
     for i in range(len(reference)):
         scores = rouge.get_scores(summary[i], reference[i])
-        avg += scores["rouge-1"]["f"]
+        avg += scores[0]["rouge-1"]["f"]
 
     return avg / len(reference)
 
 def evaluate(dictionary, corpus, reference, threshold):
 
+    score = 0.0
+
     if corpus == "train":
         for file in os.listdir("dataset/body"):
             filename = f"dataset/body/{os.fsdecode(file)}"
 
-            summary = summarize(dictionary, filename, 0.0)
-            print(summary)
+            summary = summarize(dictionary, filename, 0.6)
+            score += score_summary(summary, f"dataset/highlights/{os.fsdecode(file)}")
+            
             break
 
-    return {0.0}
+    return {score}
