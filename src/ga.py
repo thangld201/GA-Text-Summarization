c23aa510940156c0233e0f5022d71d8db14c9ed9
William Chen <wchen6255@knights.ucf.edu>
2021-04-24 00:00:46 -0400

    sort of working ga

--- src/ga.py
+++ src/ga.py
@@ -2,21 +2,23 @@ import os
 
 from rouge import Rouge 
 from sacremoses import MosesTokenizer
+from dictionary import create_dictionary
 
 def load_corpus(mode):
     articles = list()
     highlights = list()
 
-
     for in_file in os.listdir(f"dataset/{mode}/body"):
 
-        filename = f"dataset/train/{mode}/{os.fsdecode(in_file)}"
+        filename = f"dataset/{mode}/body/{os.fsdecode(in_file)}"
         articles.append(open(filename, 'r', encoding='utf-8').readlines())
+        break
 
     for in_file in os.listdir(f"dataset/{mode}/highlights"):
 
         filename = f"dataset/{mode}/highlights/{os.fsdecode(in_file)}"
         highlights.append(open(filename, 'r', encoding='utf-8').readlines())
+        break
 
     return articles, highlights
 
@@ -25,45 +27,47 @@ def summarize(dictionary, corpus, threshold):
     tokenizer = MosesTokenizer(lang='en')
     summary = list()
 
-    with open(corpus, 'r', encoding='utf-8') as file:
-        corpus_lines = file.readlines()
+    for line in corpus:
 
-        for line in corpus_lines:
+        line_tok = tokenizer.tokenize(line)
 
-            line_tok = tokenizer.tokenize(line)
-
-            sentence_score = 0.0
-            for word in line_tok:
-                if word.lower() in dictionary:
-                    sentence_score += dictionary[word.lower()]
-                if sentence_score / len(line_tok) > threshold:
-                    summary.append(line)
-                    break
+        sentence_score = 0.0
+        for word in line_tok:
+            if word.lower() in dictionary:
+                sentence_score += dictionary[word.lower()]
+            if sentence_score / len(line_tok) > threshold:
+                summary.append(line)
+                break
 
     return summary
 
-def score_summary(summary, reference_file):
+def score_summary(summary, reference):
     avg = 0.0
     rouge = Rouge()
-    reference = open(reference_file, 'r', encoding='utf-8').readlines()
+    length = len(reference)
 
-    for i in range(len(reference)):
+    if len(summary) <= 0:
+        return 0
+
+    for i in range(length):
         scores = rouge.get_scores(summary[i], reference[i])
         avg += scores[0]["rouge-1"]["f"]
 
     return avg / len(reference)
 
-def evaluate(dictionary, corpus, threshold):
+def evaluate(dictionary, articles, highlights, threshold):
 
     score = 0.0
+    length = len(articles)
+    for i in range(length):
+        summary = summarize(dictionary, articles[i], threshold)
+        score += score_summary(summary, highlights[i])
 
-    if corpus == "train":
-        for in_file in os.listdir("dataset/body"):
-            filename = f"dataset/body/{os.fsdecode(file)}"
+        break
 
-            summary = summarize(dictionary, filename, threshold)
-            score += score_summary(summary, f"dataset/highlights/{os.fsdecode(in_file)}")
-            
-            break
+    return {score / length}
 
-    return {score / len(dictionary)}
+def evaluate_ga(vocab, individual, articles, highlights, threshold):
+    dictionary = create_dictionary(vocab, individual)
+
+    return evaluate(dictionary, articles, highlights, threshold)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import random
 
 from deap import creator, base, tools, algorithms
-from ga import evaluate
+from ga import evaluate_ga, load_corpus
 from dictionary import read_vocab, create_dictionary
 
 # set up objective
 creator.create("ROUGEFitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.ROUGEFitnessMax)
 
-# read in vocab
+# read in vocab and dataset
 vocab = read_vocab()
+print("vocabulary loaded")
+articles, highlights = load_corpus("test")
+print("dataset loaded")
 
 # chromosome length
 IND_SIZE=len(vocab)
@@ -20,11 +23,48 @@ toolbox.register("attr_float", random.random)
 toolbox.register("individual", tools.initRepeat, creator.Individual,
                  toolbox.attr_float, n=IND_SIZE)
 
-# initialize an individual
-ind1 = toolbox.individual()
-dictionary1 = create_dictionary(vocab, ind1)
+# set up population
+POP_SIZE = 100
+pop = list()
+for i in range(POP_SIZE):
+    pop.append(toolbox.individual())
 
+print("Population Initialized")
 
-ind1.fitness.values = evaluate(dictionary1, "train", 0.6)
+# set up crossover
+CXPB = 0.8
+MUTPB = 0.05
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("select", tools.selTournament, tournsize=5)
 
-print(ind1.fitness)
+num_generations = 100
+threshold = 0.2
+for i in range(num_generations):
+    print(f"Generation: {i}")
+    # Select the next generation individuals
+    offspring = toolbox.select(pop, POP_SIZE)
+    # Clone the selected individuals
+    offspring = list(map(toolbox.clone, offspring))
+
+    # Apply crossover on the offspring
+    for child1, child2 in zip(offspring[::2], offspring[1::2]):
+        if random.random() < CXPB:
+            toolbox.mate(child1, child2)
+            del child1.fitness.values
+            del child2.fitness.values
+
+    # Apply mutation on the offspring
+    '''
+    for mutant in offspring:
+        if random.random() < MUTPB:
+            toolbox.mutate(mutant)
+            del mutant.fitness.values
+    '''
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    for ind in invalid_ind:
+        ind.fitness.values = evaluate_ga(vocab, ind, articles, highlights, threshold)
+
+    # The population is entirely replaced by the offspring
+    pop[:] = offspring
